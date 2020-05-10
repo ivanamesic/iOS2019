1e031307c6d2a8b692168f1a1af013fba035c210
imesic <ivana_mesic123@hotmail.com>
2020-05-10 19:13:37 +0200

    Initial Commit

---
+++ Quiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quiz
+//
+//  Created by five on 10/05/2020.
+//  Copyright © 2020 Ivana Mesic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
